7a87b24fae47bb2bf5d0ef18e1451a1c611cf7b5
Darryl-Shi <221474B@student.hci.edu.sg>
2022-03-13 22:34:52 +0800

    i have lost my mind. ill continue in the morning

--- src/addquestion.py
+++ src/addquestion.py
@@ -6,8 +6,7 @@ connection = sqlite3.connect("./database.db")
 cur = connection.cursor()
 
 # create database table
-cur.execute('''CREATE TABLE IF NOT EXISTS questions(category TEXT NOT NULL, id INTEGER NOT NULL, level INTEGER NOT NULL, question TEXT NOT NULL, 
-                options TEXT NOT NULL, answer INTEGER NOT NULL, explanation TEXT NOT NULL)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS questions(category TEXT NOT NULL, level INTEGER NOT NULL, question TEXT NOT NULL, options TEXT NOT NULL, answer INTEGER NOT NULL, explanation TEXT NOT NULL)''')
 
 # Flask app
 app = Flask(__name__)
@@ -17,23 +16,23 @@ def home():
     return "<h1>hello, world!</h1>"
 
 
-def addquestion():
-    id = ""
-    question = ""
-    level = ""
-    category = ""
-    options = []
-    answer = "" #maybe ans should be like a string like option number
-    explanation = ""
+
+
+question = ""
+level = ""
+category = ""
+options = []
+answer = "" #maybe ans should be like a string like option number
+explanation = ""
 
 
     #yuan cheng do some html magic and make this happen
 
 question_values = (
-            (category, id, level, question, options, answer, explanation)
+            (category, level, question, options, answer, explanation)
         )
-def upload_to_sql(i):
-    cur.execute("insert into questions values (category, id, level, question, options, answer, explanation)",question_values)
+def upload_to_sql():
+    cur.execute("insert into questions values (category, level, question, options, answer, explanation)",question_values)
 
 
 if __name__ == "__main__":
