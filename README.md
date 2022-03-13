646323fcb2940e1cd5cacf6ca87ab3c9b7c09dea
Sun Zizhuo <211530w@student.hci.edu.sg>
2022-03-13 17:26:25 +0800

    first commit for boilerplate code
    also start working on things thanks

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Backend
+Nothing much, just the backend server for the project. Remcber to install `Flask` via running `pip install flask`. You can install [DB Browser](https://sqlitebrowser.org) SQLite to view the database and make the process easier.
---
+++ src/main.py
@@ -0,0 +1,20 @@
+import sqlite3
+from flask import Flask
+
+# Flask app
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>hello, world!</h1>"
+
+if __name__ == "__main__":
+    app.run()
+
+# create sqlite connection
+connection = sqlite3.connect("./src/database.db")
+cur = connection.cursor()
+
+# create database table
+cur.execute('''CREATE TABLE IF NOT EXISTS questions(category TEXT NOT NULL, level INTEGER NOT NULL, question TEXT NOT NULL, 
+                options TEXT NOT NULL, answer INTEGER NOT NULL, explanation TEXT NOT NULL)''')
