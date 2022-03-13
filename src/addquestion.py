90dfe60f0d3e830720912acad8876af6d6d93a01
Darryl-Shi <221474B@student.hci.edu.sg>
2022-03-13 20:42:02 +0800

    added some code to publish question to sql db

---
+++ src/addquestion.py
@@ -0,0 +1,40 @@
+import sqlite3
+from flask import Flask
+
+# create sqlite connection
+connection = sqlite3.connect("./database.db")
+cur = connection.cursor()
+
+# create database table
+cur.execute('''CREATE TABLE IF NOT EXISTS questions(category TEXT NOT NULL, id INTEGER NOT NULL, level INTEGER NOT NULL, question TEXT NOT NULL, 
+                options TEXT NOT NULL, answer INTEGER NOT NULL, explanation TEXT NOT NULL)''')
+
+# Flask app
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>hello, world!</h1>"
+
+
+def addquestion():
+    id = ""
+    question = ""
+    level = ""
+    category = ""
+    options = []
+    answer = "" #maybe ans should be like a string like option number
+    explanation = ""
+
+
+    #yuan cheng do some html magic and make this happen
+
+question_values = (
+            (category, id, level, question, options, answer, explanation)
+        )
+def upload_to_sql(i):
+    cur.execute("insert into people values (category, id, level, question, options, answer, explanation)",question_values)
+
+
+if __name__ == "__main__":
+    app.run()
