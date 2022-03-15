86efe1bd09049906657ce35a8b51429d3048b5fb
56cy <88962938+56cy@users.noreply.github.com>
2022-03-15 14:32:27 +0800

    update src
    i hope i added all the files correctly

---
+++ src/database_columns.py
@@ -0,0 +1,7 @@
+# Database columns.
+
+QUESTION_ADD_COLUMNS = ("category", "level", "question", "option1", "option2", "option3", "option4", "answer", "explanation")
+
+QUESTION_GET_COLUMNS = ("mode", "num", "userid")
+
+DEFINITION_ADD_COLUMNS = ("keyword", "aliases", "definition")
