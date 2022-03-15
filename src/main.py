e9fdf354031ddde6a0f71ae6dbd1efba484770f6
Sun Zizhuo <211530w@student.hci.edu.sg>
2022-03-15 10:16:59 +0800

    made it such that one url can accept both post and get requests (boilerplate code only)

--- src/main.py
+++ src/main.py
@@ -21,37 +21,45 @@ app = Flask(__name__)
 def home():
     return "<h1>hello, world!</h1>"
 
-@app.route("/api/v1/add_question", methods = ["POST"])
-def add_question():
+@app.route("/api/v1/questions", methods = ["GET", "POST"])
+def question():
+    # If the request method is POST, add question; if request method is GET return the number of questions as stated in query string
+    if request.method == "POST":
+        # get post data
+        content = dict(request.form)
+        print(content)
 
-    # get post data
-    content = dict(request.form)
-    print(content)
+        # raise error if keys not provided
+        if not dictContains(("category", "level", "question", "option1", "option2", "option3", "option4", "answer", "explanation"), content):
+            return "MISSING_VALUES"
 
-    # raise error if keys not provided
-    if not dictContains(("category", "level", "question", "option1", "option2", "option3", "option4", "answer", "explanation"), content):
-        return "MISSING_VALUES"
+        # raise error if datas are of wrong type
+        if not convertDictTypes((str, int, str, str, str, str, str, int, str), content):
+            return "WRONG_TYPES"
 
-    # raise error if datas are of wrong type
-    if not convertDictTypes((str, int, str, str, str, str, str, int, str), content):
-        return "WRONG_TYPES"
+        # insert query
+        safeInsert(
+            cur,
+            content["category"],
+            content["level"],
+            content["question"],
+            content["option1"], content["option2"], content["option3"], content["option4"],
+            content["answer"],
+            content["explanation"]
+        )
 
-    # insert query
-    safeInsert(
-        cur,
-        content["category"],
-        content["level"],
-        content["question"],
-        content["option1"], content["option2"], content["option3"], content["option4"],
-        content["answer"],
-        content["explanation"]
-    )
+        # commit
+        connection.commit()
 
-    # commit
-    connection.commit()
+        # return result
+        return "SUCCESS"
+    elif request.method == "GET":
+        
+        # Checks if the number of questions passed into the query string is an integer
+        # The number of questions returned will correspond to the number of questions
+        # If not an integer, the number of questions returned will default to 1
 
-    # return result
-    return "SUCCESS"
+        return "SUCCESS"
 
 if __name__ == "__main__":
     app.run()
