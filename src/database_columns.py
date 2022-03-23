be4098ebcfc75a7202cf4e8d8b16907c6ed5a771
horse-3903 <86842464+horse-3903@users.noreply.github.com>
2022-03-23 08:04:01 +0800

    Update database_columns.py

--- src/database_columns.py
+++ src/database_columns.py
@@ -2,4 +2,4 @@
 QUESTION_ADD_COLUMNS = ("category", "level", "question", "option1", "option2", "option3", "option4", "answer", "explanation","id")
 QUESTION_GET_COLUMNS = ("mode", "num", "userid")
 DEFINITION_ADD_COLUMNS = ("item", "definition", "aliases")
-DEFINITION_GET_COLUMNS = ("input",)
+DEFINITION_GET_COLUMNS = ("input")
